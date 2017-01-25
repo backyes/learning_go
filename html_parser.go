958e46eb87286b01d9429df0444b2c5b3e21a831
backyes <backyes@gmail.com>
2017-01-25 14:54:33 +0800

    hacking html_parser(Continue)

--- html_parser.go
+++ html_parser.go
@@ -5,20 +5,47 @@ package main
 import (
   "flag"
   "log"
+  "github.com/djimenez/iconv-go"
   "github.com/PuerkitoBio/goquery"
 )
 
-func Get(url string) {
-  doc, err := goquery.NewDocument(url)
-  if err != nil {
-    log.Fatal("go query NewDocument failed")
-  }
+func html_parser(url string) {
+     // Load the URL
+     res, err := http.Get(url)
+     if err != nil {
+       log.Fatal("load http URL error")
+         // handle error
+     }
+     defer res.Body.Close()
 
-  dhead := doc.Find("head")
-  dcharset := dhead.Find("meta[http-equiv]")
-  charset, _ := dcharset.Attr("content")
+     // Convert the designated charset HTML to utf-8 encoded HTML.
+     // `charset` being one of the charsets known by the iconv package.
+     utfBody, err := iconv.NewReader(res.Body, charset, "utf-8")
+     if err != nil {
+       log.Fatal("convert to utf-8 eror")
+         // handler error
+     }
 
-  log.Println(charset)
+     // use utfBody using goquery
+     doc, err := goquery.NewDocumentFromReader(utfBody)
+     if err != nil {
+       log.Fatal("create new document for goquery failed")
+         // handler error
+     }
+     // use doc...
+}
+
+func GetCharset(url string) {
+     doc, err := goquery.NewDocument(url)
+     if err != nil {
+       log.Fatal("go query NewDocument failed")
+     }
+
+     dhead := doc.Find("head")
+     dcharset := dhead.Find("meta[http-equiv]")
+     charset, _ := dcharset.Attr("content")
+
+     log.Println(charset)
 }
 
 func main() {
